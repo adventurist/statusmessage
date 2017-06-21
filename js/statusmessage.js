0a4daeef7ee5724599136a4fb6458c6b9943a037
logicp <emmanuel@openconcept.ca>
2017-06-20 21:54:20 -0400

    quick fix

--- js/statusmessage.js
+++ js/statusmessage.js
@@ -44,7 +44,7 @@
       };
 
       function validateUrl(input) {
-        if (null != null) {
+        if (input !== null) {
           return input.match(new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?"));
         }
       }
